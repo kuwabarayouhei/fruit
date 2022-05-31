d03b5e6e1570cd2b2aa47b6e7a582661329d67f0
kuwabarayouhei <kuwayou.syukatu@gmail.com>
2022-05-31 14:19:25 +0900

    掲示板投稿の削除

---
+++ blog/app/Post.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    
+    use SoftDeletes;
+    
+    protected $fillable = [
+    'title',
+    'model',
+    'number',
+    'color',
+    'location',
+    'time',
+    'situation',
+    'information',
+];
+    
+    public function getByLimit(int $limit_count = 10)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+
+    
+}
---
+++ blog/resources/views/show.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html lang="{{ str_replace("_", "-", app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Posts</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        <h1 class="title">
+            {{ $post->title }}
+        </h1>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+        @csrf
+        @method('DELETE')
+    <button type="submit">delete</button>
+    <script src="./js/fruit.js"></script>
+</form>
+        <div class="content">
+            <div class="content__post">
+                <h3>車種</h3>
+                <p>{{ $post->model }}</p>
+                <h3>ナンバープレート</h3>
+                <p>{{ $post->number }}</p>
+                <h3>色</h3>
+                <p>{{ $post->color }}</p>
+                <h3>盗難された場所</h3>
+                <p>{{ $post->location }}</p>  
+                <h3>盗難された時間</h3>
+                <p>{{ $post->time }}</p>
+                <h3>盗難時の状況</h3>
+                <p>{{ $post->situation }}</p>
+                <h3>その他の情報</h3>
+                <p>{{ $post->Information }}</p>
+            </div>
+        </div>
+        <div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
